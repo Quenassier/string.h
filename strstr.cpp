ba226347f47260f6cb65a5959c4d10dacfd4905c
Quenassier <112952435+Quenassier@users.noreply.github.com>
2023-10-25 22:44:00 +0700

    Create strstr.cpp

---
+++ strstr.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+const char* my_strstr(const char* haystack, const char* needle) {
+    if (!haystack || !needle) {
+        return nullptr;
+    }
+
+    for (int i = 0; haystack[i] != '\0'; i++) {
+        int j = 0;
+        while (needle[j] != '\0' && haystack[i + j] == needle[j]) {
+            j++;
+        }
+
+        if (needle[j] == '\0') {
+            return &haystack[i];
+        }
+    }
+
+    return nullptr;
+}
+
+int main() {
+    const char* haystack = "This is a sample text.";
+    const char* needle = "sample";
+
+    const char* result = my_strstr(haystack, needle);
+
+    if (result) {
+        std::cout << "Substring found at index: " << (result - haystack) << std::endl;
+    } else {
+        std::cout << "Substring not found." << std::endl;
+    }
+
+    return 0;
+}
