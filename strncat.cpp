6ae8ef5d17c48deedb9f387007c9bf068a4804c5
Quenassier <112952435+Quenassier@users.noreply.github.com>
2023-10-25 22:38:21 +0700

    Create strncat.cpp

---
+++ strncat.cpp
@@ -0,0 +1,23 @@
+include <iostream>
+void myStrncat(char* destination, const char* source, size_t num) {
+
+    while (*destination) {
+        destination++;
+    }
+        while (*source && num--) {
+        *destination++ = *source++;
+    }
+
+    *destination = '\0';
+}
+
+int main() {
+    char destination[100] = "Hello, ";
+    const char* source = "world!";
+    size_t num = 5;
+    
+    myStrncat(destination, source, num);
+    
+    std::cout << destination << std::endl;
+    return 0;
+}
